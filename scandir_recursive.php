892ab563874f4b437ce979caf4c80310af04e79f
bejo6 <bejo6.er@gmail.com>
2018-07-06 11:04:44 +0700

    Update scandir_recursive.php

--- scandir_recursive.php
+++ scandir_recursive.php
@@ -1,4 +1,28 @@
 <?php
+
+function scandir_recursive($path){
+    $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
+
+    $res = array(); 
+
+    foreach ($rii as $list) {
+
+        if(substr($list->getPathname(), -2) == '/.'){
+            continue;
+        }
+        if ($list->isDir()){
+            $dir = substr($list->getPathname(),0, -3);
+            if($dir != $path){
+                $res['dir'][] = $dir;
+            }
+        }else{
+            $res['file'][] = $list->getPathname(); 
+        }
+    }
+
+    return $res;
+}
+
 /*
 Usage :
 
@@ -28,25 +52,3 @@ Array
 )
 
 */
-function scandir_recursive($path){
-    $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
-
-    $res = array(); 
-
-    foreach ($rii as $list) {
-
-        if(substr($list->getPathname(), -2) == '/.'){
-            continue;
-        }
-        if ($list->isDir()){
-            $dir = substr($list->getPathname(),0, -3);
-            if($dir != $path){
-                $res['dir'][] = $dir;
-            }
-        }else{
-            $res['file'][] = $list->getPathname(); 
-        }
-    }
-
-    return $res;
-}
