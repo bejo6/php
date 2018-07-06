cf002e3aacdccf1ae25fe74e7b2c956ac7138830
bejo6 <bejo6.er@gmail.com>
2018-07-06 10:56:56 +0700

    Update scandir_recursive.php
    Usage :
    
    $path = "/your/path/";
    $list = scandir_recursive($path);
    print_r($list);

--- scandir_recursive.php
+++ scandir_recursive.php
@@ -1,28 +1,23 @@
 <?php
 function scandir_recursive($path){
-	$rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
+    $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
 
-	$res = array(); 
+    $res = array(); 
 
-	foreach ($rii as $list) {
+    foreach ($rii as $list) {
 
-		if(substr($list->getPathname(), -2) == '/.'){
-			continue;
-		}
-		if ($list->isDir()){
-			$dir = substr($list->getPathname(),0, -3);
-			if($dir != $path){
-				$res['dir'][] = $dir;
-			}
-		}else{
-			$res['file'][] = $list->getPathname(); 
-		}
-	}
+        if(substr($list->getPathname(), -2) == '/.'){
+            continue;
+        }
+        if ($list->isDir()){
+            $dir = substr($list->getPathname(),0, -3);
+            if($dir != $path){
+                $res['dir'][] = $dir;
+            }
+        }else{
+            $res['file'][] = $list->getPathname(); 
+        }
+    }
 
-	return $res;
+    return $res;
 }
-
-
-$path = "/your/path/";
-$list = scandir_recursive($path);
-print_r($list);
